b5d6aa31e5ef8580d03a7b801239e57bfb0bf7fd
Pedro Rodrigues <medecau@gmail.com>
2012-08-23 16:55:31 +0100

    0.0.6

--- setup.py
+++ setup.py
@@ -1,24 +1,20 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
-import packets
-
-#with open('README.rst') as f:
-#    readme = f.read()
-readme = "nothing to see here, move along"
 
 with open('LICENSE') as f:
     license = f.read()
 
 setup(
     name='packets',
-    version=packets.__version__,
+    version='0.0.6',
     description='UDP for Humans',
-    long_description=readme,
-    author=packets.__author__,
+    author='Pedro Rodrigues',
     author_email='medecau@gmail.com',
     url='http://medecau.github.com/packets',
     license=license,
-    packages=find_packages(exclude=('test'))
+    pu_modules=['travis'],
+    packages=find_packages(exclude=('test')),
+    #install_requires=['clint',],
 )
 
